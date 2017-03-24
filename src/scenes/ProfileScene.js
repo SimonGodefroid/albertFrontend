d0e01cef5dad598bb768af979268096f9de7c49a
SimonGodefroid <simon.godefroid@gmail.com>
2017-03-24 16:19:44 +0100

    fixed redirection after click on see profile, started profilescene added dimension shortcut to MapProduct

--- src/scenes/ProfileScene.js
+++ src/scenes/ProfileScene.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
+  Dimensions,
   Image,
   View,
   ScrollView,
@@ -22,10 +23,58 @@ import Button from '../components/core/Button';
 import Favorites from '../components/user/Favorites';
 import UserImage from '../components/user/UserImage';
 
+
+let {
+  height,
+  width,
+} = Dimensions.get('window');
+
+
+
 class ProfileScene extends React.Component {
   render() {
     return(
-      <View>
+      <View 
+        style={
+          {flex:1,
+          alignItems:'center'}
+        }>
+        <View>
+          <UserImage
+            image={this.props.image}
+            style={{borderRadius:25}}
+          />
+        </View>
+        <View>
+          <Text> Coucou </Text>
+        </View>
+        <View 
+          style={
+            {
+              height:100,
+              width:width-20,
+              borderColor:'black',
+              borderWidth:2,
+              flexDirection:'row',
+              justifyContent:'space-between'
+            }
+        }>
+          <View>
+            <Text>
+              Clément{'\n'}
+            </Text>
+          </View>
+          <View>
+            <Text>
+              Clément
+            </Text>
+          </View>
+          <View>
+            <Text>
+              Clément
+            </Text>
+          </View>
+        </View>
       </View>
     );
   }
