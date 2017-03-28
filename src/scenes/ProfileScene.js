4583914cdfd7bc08d49932c79015de6b920db167
SimonGodefroid <simon.godefroid@gmail.com>
2017-03-28 14:43:49 +0200

    added swiper to profile page

--- src/scenes/ProfileScene.js
+++ src/scenes/ProfileScene.js
@@ -22,6 +22,8 @@ import Global from '../Global';
 import Button from '../components/core/Button';
 import Favorites from '../components/user/Favorites';
 import UserImage from '../components/user/UserImage';
+import Swiper from 'react-native-swiper';
+
 
 
 let {
@@ -47,47 +49,81 @@ class ProfileScene extends React.Component {
         </View>
       </Image>
       <View style={{flexDirection:'row',flex:1}}>
-        <View style={styles.profile_elements}>
-          <Text>Tags:</Text>
+        
         </View>
-         <View style={styles.profile_elements}>
-          <Text>Favoris:</Text>
+      <Swiper style={styles.wrapper} showsButtons={true}>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Hello Swiper</Text>
         </View>
-      </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Beautiful</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+      </Swiper>
       </ScrollView>
-
     );
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingTop: 60,
-      alignItems: 'center',
-      width:null,
-      resizeMode: 'cover',
-      padding: 20,
-      height:300
-    },
-    profile_container: {
-      alignItems: 'center'
-    },
-    name_desc: {
-      paddingTop:10,
-      alignItems: 'center', 
-      backgroundColor: 'transparent'
-    },
-    user_image: {
-      borderRadius:75,
-      width:150,
-      height:150,
-    },
-    profile_elements:{
-      flex:1,
-      padding:10
-    }
+  container: {
+    flex: 1,
+    paddingTop: 60,
+    alignItems: 'center',
+    width:null,
+    resizeMode: 'cover',
+    padding: 20,
+    height:300
+  },
+  profile_container: {
+    alignItems: 'center'
+  },
+  name_desc: {
+    paddingTop:10,
+    alignItems: 'center', 
+    backgroundColor: 'transparent'
+  },
+  user_image: {
+    borderRadius:75,
+    width:150,
+    height:150,
+  },
+  profile_elements:{
+    flex:1,
+    padding:10
+  },  
+  wrapper: {
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
 });
+
+
+
+
 
 
 export default ProfileScene;
