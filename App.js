8e44ac4377cb20f7fdc471abe063c81ba23a5088
Clem <Clem>
2017-03-29 02:01:35 +0200

    fixed login routes and conditions -clem

--- App.js
+++ App.js
@@ -29,6 +29,7 @@ import EventScene from './src/scenes/EventScene';
 import PlaceScene from './src/scenes/PlaceScene';
 import TabIcon from './src/components/core/TabIcon';
 import Api from './src/Api';
+import Loading from './src/components/core/Loading';
 
 class App extends React.Component {
 	constructor(props) {
@@ -46,22 +47,31 @@ class App extends React.Component {
 		})
 	}
 	render() {
-		return (
-			<Router leftButtonIconStyle={{
-				tintColor: 'white'
-			}} navigationBarStyle={{
-				backgroundColor: '#5bbcb2'
-			}} hideNavBar={true}>
-				<Scene key={'signup'} component={SignupScene}/>
-				<Scene key={'login'} component={LoginScene} initial={this.state.user === null ? true : false}/>
-				<Scene key={'search'} component={SearchScene} initial={this.state.user !== null ? true : false}/>
-				<Scene key={'profile'} component={ProfileScene}/>
-				<Scene key={'editProfile'} component={EditProfileScene}/>
-				<Scene key={'results'} component={ResultsScene}/>
-				<Scene key={'event'} component={EventScene}/>
-				<Scene key={'place'} component={PlaceScene}/>
-			</Router>
-		);
+		console.log(this.state.user)
+		if (this.state.user === undefined) {
+			console.log("loading...", this.state.user)
+			return (
+				<Loading/>
+			)
+		} else {
+			console.log("identified user...", this.state.user)
+			return (
+				<Router leftButtonIconStyle={{
+					tintColor: 'white'
+				}} navigationBarStyle={{
+					backgroundColor: '#5bbcb2'
+				}} hideNavBar={true}>
+					<Scene key={'signup'} component={SignupScene}/>
+					<Scene key={'login'} component={LoginScene} initial={this.state.user === null ? true : false}/>
+					<Scene key={'search'} component={SearchScene} initial={this.state.user !== null ? true : false}/>
+					<Scene key={'profile'} component={ProfileScene}/>
+					<Scene key={'editProfile'} component={EditProfileScene}/>
+					<Scene key={'results'} component={ResultsScene}/>
+					<Scene key={'event'} component={EventScene}/>
+					<Scene key={'place'} component={PlaceScene}/>
+				</Router>
+			);
+		}
 	}
 }
 
